82d130edf8f5a1a04dd9d9c5e9563a2e653c854d
ammara1000 <ammara1000@outlook.fr>
2025-07-20 14:29:46 +0200

    idk

--- src/main/java/nerd/amara/Francetiers_tagger.java
+++ src/main/java/nerd/amara/Francetiers_tagger.java
@@ -1,24 +1,25 @@
 package nerd.amara;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientEntityEvents;
+import net.minecraft.entity.player.PlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Francetiers_tagger implements ModInitializer {
+public class Francetiers_tagger implements ClientModInitializer {
 	public static final String MOD_ID = "francetiers_tagger";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+	public void onInitializeClient() {
+		ClientEntityEvents.ENTITY_LOAD.register(((entity, clientWorld) ->{
+			if (entity instanceof PlayerEntity){
+				((TierModifier)entity).setSuffix(" (lol)");
+			}
+		}));
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("france tiers tagger initialized");
 	}
 }
---
+++ src/main/java/nerd/amara/mixin/MixinPlayerEntity.java
@@ -0,0 +1,28 @@
+package nerd.amara.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import nerd.amara.TierModifier;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(PlayerEntity.class)
+public class MixinPlayerEntity implements TierModifier {
+    private String suffix = "";
+    @Override
+    public String getSuffix(){
+        return suffix;
+    }
+    @Override
+    public void setSuffix(String value){
+        this.suffix=value;
+    }
+    @ModifyReturnValue(method = "getDisplayName", at = @At("RETURN"))
+    public Text lol(Text original) {
+        PlayerEntity self = (PlayerEntity) (Object) this;
+        //String username = self.getName().getString();
+        String sufix = this.suffix;
+        return original.copy().append(Text.literal(" "+sufix));
+    }
+}
